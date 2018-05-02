e42c9c5a69311c994083a5ac3487b829cd7007e6
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:11:37 +0200

    Update SchuheDE.php

--- src/ResultField/SchuheDE.php
+++ src/ResultField/SchuheDE.php
@@ -94,7 +94,7 @@ class SchuheDE extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
 		/**
 		 * @var BarcodeMutator $barcodeMutator
